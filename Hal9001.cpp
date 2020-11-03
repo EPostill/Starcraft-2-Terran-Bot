afd326089c1c0d7224bfdef0462ca954b0417eff
EPostill <46463869+EPostill@users.noreply.github.com>
2020-11-03 11:04:29 -0700

    Changed name

---
+++ Hal9001.cpp
@@ -0,0 +1,5 @@
+#include "Hal9001.h"
+
+void Hal9001::OnGameStart() { return; }
+
+void Hal9001::OnStep() { return; }
--- Hal9001.h
+++ Hal9001.h
@@ -1,5 +1,5 @@
-#ifndef BASIC_SC2_BOT_H_
-#define BASIC_SC2_BOT_H_
+#ifndef HAL_9001_H_
+#define HAL_9001_H_
 
 #include "sc2api/sc2_api.h"
 #include "sc2api/sc2_args.h"
@@ -7,7 +7,7 @@
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
 
-class BasicSc2Bot : public sc2::Agent {
+class Hal9001 : public sc2::Agent {
 public:
 	virtual void OnGameStart();
 	virtual void OnStep();
