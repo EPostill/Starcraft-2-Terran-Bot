f563f449d2085e5a151df511f22dea9baf6a9fd5
Michelle Aubin <phiangda@ualberta.ca>
2020-11-03 11:48:30 -0700

    working code from michelle branch

--- Hal9001.h
+++ Hal9001.h
@@ -7,10 +7,20 @@
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
 
+using namespace sc2;
+
 class Hal9001 : public sc2::Agent {
 public:
 	virtual void OnGameStart();
 	virtual void OnStep();
+
+	virtual void OnUnitIdle(const Unit* unit);
+	
+	const Unit* FindNearestMineralPatch(const Point2D &start);
+	const Unit* FindNearestGeyser(const Point2D &start);
+	bool TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
+	bool TryBuildSupplyDepot();
+	bool TryBuildRefinery();
 
 private:
 };
