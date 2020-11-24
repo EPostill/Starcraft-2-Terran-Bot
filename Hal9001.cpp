779524bf795081a19f1203bb055012abec0f4aba
Michelle Aubin <phiangda@ualberta.ca>
2020-11-24 13:32:40 -0700

    updated findNearest functions

--- Hal9001.cpp
+++ Hal9001.cpp
@@ -458,36 +458,42 @@ const Point3D Hal9001::FindNearestExpansion(){
 
 // returns nearest mineral patch or nullptr if none found
 const Unit* Hal9001::FindNearestMineralPatch(const Point2D &start) {
-    // gets all neutral units
-    Units units = Observation()->GetUnits(Unit::Alliance::Neutral);
+    Units units = GetUnitsOfType(UNIT_TYPEID::NEUTRAL_MINERALFIELD);
     float distance = std::numeric_limits<float>::max();
     const Unit *target = nullptr;
     for (const auto &u : units) {
-        // get closest mineral field
-        if (u->unit_type == UNIT_TYPEID::NEUTRAL_MINERALFIELD) {
-            float d = DistanceSquared2D(u->pos, start);
-            if (d < distance) {
-                distance = d;
-                target = u;
-            }
+        float d = DistanceSquared2D(u->pos, start);
+        if (d < distance) {
+            distance = d;
+            target = u;
         }
     }
     return target;
 }
 // returns nearest vespene geyser or nullptr if none found
 const Unit* Hal9001::FindNearestGeyser(const Point2D &start) {
-    // gets all neutral units
-    Units units = Observation()->GetUnits(Unit::Alliance::Neutral);
+    Units units = GetUnitsOfType(UNIT_TYPEID::NEUTRAL_VESPENEGEYSER);
     float distance = std::numeric_limits<float>::max();
     const Unit *target = nullptr;
     for (const auto &u : units) {
-        // get closest vespene geyser
-        if (u->unit_type == UNIT_TYPEID::NEUTRAL_VESPENEGEYSER) {
-            float d = DistanceSquared2D(u->pos, start);
-            if (d < distance) {
-                distance = d;
-                target = u;
-            }
+        float d = DistanceSquared2D(u->pos, start);
+        if (d < distance) {
+            distance = d;
+            target = u;
+        }
+    }
+    return target;
+}
+
+const Unit* Hal9001::FindNearestDepot(const Point2D &start) {
+    Units units = getDepots();
+    float distance = std::numeric_limits<float>::max();
+    const Unit *target = nullptr;
+    for (const auto &u : units) {
+        float d = DistanceSquared2D(u->pos, start);
+        if (d < distance) {
+            distance = d;
+            target = u;
         }
     }
     return target;
--- Hal9001.h
+++ Hal9001.h
@@ -31,6 +31,8 @@ public:
 	const Unit* FindNearestMineralPatch(const Point2D &start);
 	// returns geyser nearest to start
 	const Unit* FindNearestGeyser(const Point2D &start);
+	// returns supply depot nearest to start
+	const Unit* FindNearestDepot(const Point2D &start);
 	// returns free expansion location nearest to main base
 	const Point3D FindNearestExpansion();
 	void BuildStructure(ABILITY_ID ability_type_for_structure, float x, float y, const Unit *builder = nullptr);
